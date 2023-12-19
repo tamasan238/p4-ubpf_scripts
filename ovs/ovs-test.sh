8a4fdde3220a0ff0eadf80c5a76077623420cfb7
tamasan238 <m1117@aol.jp>
2023-12-20 03:02:14 +0900

    apply icmp filter for tcpdump

--- ovs/ovs-test.sh
+++ ovs/ovs-test.sh
@@ -31,7 +31,7 @@ ip link set $BRIDGE_NAME up
 ip -br a
 
 # tcpdump -i $IFACE_NAME &
-tcpdump -v -i $BRIDGE_NAME >> $PCAP_FILE &
+tcpdump icmp -v -i $BRIDGE_NAME >> $PCAP_FILE &
 TCPDUMP_PID=$!
 
 ssh iwai@$REMOTE_IP_ADDRESS "sudo ip route add $DUMMY_IP_ADDRESS via $MY_IP_ADDRESS"
