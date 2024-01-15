4ad99899c5191ea9793aedae9860ef0c73add1e8
masaki <m1117@aol.jp>
2024-01-15 17:28:04 +0900

    fix around ext

--- p4/prepare.sh
+++ p4/prepare.sh
@@ -6,7 +6,7 @@ export P4FILENAME=$P4SAMPLE/action_call_ubpf
 
 # P4 -> C
 alias p4-c='p4c-ubpf $P4FILENAME.p4 -o $P4FILENAME.c'
-alias p4-c_='p4c-ubpf $P4FILENAME.p4 -o $P4FILENAME.c --emit-externs'
+alias p4-c_='p4c-ubpf $P4FILENAME\_ext.p4 -o $P4FILENAME.c --emit-externs'
 
 # C -> Assembly code (for check only)
 alias c-asm='clang -O2 -S -target bpf -c $P4FILENAME.c -o $P4FILENAME.s -I $UBPFRUNTIME'
@@ -21,7 +21,6 @@ alias run-ubpf='$UBPFBIN/ubpf_test $P4FILENAME.o'
 
 # Concat
 alias p4u='p4-c && c-asm && c-ubpf && echo $P4FILENAME && run-ubpf'
-alias p4u_='p4-c_ && c-asm_ && c-ubpf_ && echo $P4FILENAME && echo "* with _ext.c" && run-ubpf'
-
+alias p4u_='p4-c_ && c-asm_ && c-ubpf_ && echo $P4FILENAME\_ext.p4 && echo "* with _ext.c" && run-ubpf'
 EOL
 source ~/.bashrc
