63a047253ff7e31c2cc801d9a85b34a0650bc9a4
masaki <m1117@aol.jp>
2024-12-01 21:44:21 +0900

    add sudo for shm scripts

--- ovs/shm.sh
+++ ovs/shm.sh
@@ -1,4 +1,4 @@
-kill $(pgrep ivshmem-server)
-rm /dev/shm/ivshmem
-~iwai/qemu/build/contrib/ivshmem-server/ivshmem-server -F -v &
-chown libvirt-qemu:kvm /dev/shm/ivshmem /tmp/ivshmem_socket
+sudo kill $(pgrep ivshmem-server)
+sudo rm /dev/shm/ivshmem
+sudo ~iwai/qemu/build/contrib/ivshmem-server/ivshmem-server -F -v &
+sudo chown libvirt-qemu:kvm /dev/shm/ivshmem /tmp/ivshmem_socket
