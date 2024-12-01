495fe4e02113a85fa142900a38377c71117fb332
masaki <m1117@aol.jp>
2024-12-01 21:49:51 +0900

    fix shm script

--- ovs/shm.sh
+++ ovs/shm.sh
@@ -1,4 +1,4 @@
 sudo kill $(pgrep ivshmem-server)
 sudo rm /dev/shm/ivshmem
-sudo ~iwai/qemu/build/contrib/ivshmem-server/ivshmem-server -F -v &
+sudo ~iwai/qemu/build/contrib/ivshmem-server/ivshmem-server
 sudo chown libvirt-qemu:kvm /dev/shm/ivshmem /tmp/ivshmem_socket
