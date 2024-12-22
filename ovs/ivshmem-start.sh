cff1b7890ec70e1eff1de756ddf23bca77a4f054
masaki <m1117@aol.jp>
2024-12-23 05:02:51 +0900

    update

--- ovs/ivshmem-start.sh
+++ ovs/ivshmem-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo ~iwai/qemu/build/contrib/ivshmem-server/ivshmem-server
+sudo ~iwai/qemu/build/contrib/ivshmem-server/ivshmem-server -F &
 sleep 1
 sudo chown libvirt-qemu:kvm /dev/shm/ivshmem /tmp/ivshmem_socket
