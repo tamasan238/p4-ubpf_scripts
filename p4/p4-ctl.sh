26f82adbd0a24db593620e0bff7fc0697f3d4493
masaki <m1117@aol.jp>
2025-06-11 15:11:16 +0900

    add p4-ctl.sh

---
+++ p4/p4-ctl.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export P4SAMPLE=~iwai/p4c/testdata/p4_16_samples
+export UBPFBIN=~iwai/ubpf/build/bin
+export UBPFRUNTIME=~iwai/p4c/backends/ubpf/runtime
+export P4FILENAME=$P4SAMPLE/ubpf
+
+if [ $# -ne 1 ]; then
+  echo "使用法: $0 {p4-c|c-ubpf|run-ubpf|p4u|build-ubpf}"
+  exit 1
+fi
+
+case "$1" in
+  p4-c)
+    p4c-ubpf "$P4FILENAME.p4" -o "$P4FILENAME.c" --emit-externs
+    ;;
+  c-ubpf)
+    clang -O2 -target bpf -c "${P4FILENAME}_ext.c" -o "$P4FILENAME.o" \
+      -I "$UBPFRUNTIME" \
+      -include "${P4FILENAME}_ext.h" \
+      -include "$P4FILENAME.c"
+    ;;
+  run-ubpf)
+    "$UBPFBIN/ubpf_test" "$P4FILENAME.o"
+    ;;
+  p4u)
+    "$0" p4-c && "$0" c-ubpf && "$0" run-ubpf
+    ;;
+  build-ubpf)
+    sudo cmake --build build -v --target install
+    ;;
+  *)
+    echo "不正なコマンド: $1"
+    echo "使用法: $0 {p4-c|c-ubpf|run-ubpf|p4u|build-ubpf}"
+    exit 1
+    ;;
+esac
