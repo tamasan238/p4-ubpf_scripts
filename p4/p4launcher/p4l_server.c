f81c6e73cacd64e44ebcc1be4e764eeb4442ef30
masaki <m1117@aol.jp>
2025-07-13 01:01:52 +0900

    update

--- p4/p4launcher/p4l_server.c
+++ p4/p4launcher/p4l_server.c
@@ -17,12 +17,14 @@
 #include <string.h>
 #include <syslog.h>
 
-typedef struct {
+typedef struct
+{
     int ovs_thread_id;
     int p4runtime_id;
 } Connection;
 
-typedef struct {
+typedef struct
+{
     pid_t p4runtime_id;
     bool in_use;
 } P4runtime;
@@ -31,8 +33,8 @@ typedef struct {
 #define SHM_NAME "/dev/uio0"
 #define SHM_SIZE (8 * 1024 * 1024) // 8MB
 
-#define VM_AREA 0 // unused in this program
-#define META_AREA (VM_AREA + 2 * 1024 * 1024) // Start at 2MB
+#define VM_AREA 0                                  // unused in this program
+#define META_AREA (VM_AREA + 2 * 1024 * 1024)      // Start at 2MB
 #define PACKETS_AREA (META_AREA + 2 * 1024 * 1024) // Start at 4MB unused in this program
 
 #define SHM_SESSION_TABLE 0
@@ -79,7 +81,8 @@ P4runtime p4runtime[MAX_CONNECTIONS];
 // lock
 bool is_locked = false;
 
-int main() {
+int main()
+{
     openlog("P4Launcher", LOG_PID | LOG_NDELAY, LOG_DAEMON);
     daemonize();
     setup_signal_handlers();
@@ -87,20 +90,23 @@ int main() {
     shm_init();
     setup_server_socket();
 
-    for(int i=0; i<MAX_CONNECTIONS; i++){
-        p4runtime[i].p4runtime_id=-1;
-        p4runtime[i].in_use=false;
+    for (int i = 0; i < MAX_CONNECTIONS; i++)
+    {
+        p4runtime[i].p4runtime_id = -1;
+        p4runtime[i].in_use = false;
     }
 
     start_children(INIT_CLIENTS);
-    while (1) {
-        //if(available_runtimes < 4)
-        //    adjust_children();      // todo: Must change here (add 余裕)
+    while (1)
+    {
+        // if(available_runtimes < 4)
+        //     adjust_children();      // todo: Must change here (add 余裕)
 
         check_ovs();
 
         int client_fd = accept(server_sock, NULL, NULL);
-        if (client_fd < 0) {
+        if (client_fd < 0)
+        {
             syslog(LOG_ERR, "accept failure");
             continue;
         }
@@ -113,26 +119,32 @@ int main() {
     return 0;
 }
 
-void check_ovs(void){
-    for(int i=0; i<MAX_CONNECTIONS; i++){
-        if(session[i].ovs_thread_id != -1 && session[i].p4runtime_id == -1){
-            // NOT LINKED thread
+void check_ovs(void)
+{
+    for (int i = 0; i < MAX_CONNECTIONS; i++)
+    {
+        if (session[i].ovs_thread_id != -1 && session[i].p4runtime_id == -1)
+        {
             link_add(i);
         }
-        if(session[i].ovs_thread_id == -1 && session[i].p4runtime_id != -1){
-            // NOT LINKED p4runtime
+        if (session[i].ovs_thread_id == -1 && session[i].p4runtime_id != -1)
+        {
             link_del(i);
         }
     }
 }
 
-void link_add(int i){
-    if(available_runtimes < 1){
-        // wake up runtime
-        adjust_children(runtimes+1);
+void link_add(int i)
+{
+    if (available_runtimes < 1)
+    {
+        // start runtime
+        adjust_children(runtimes + 1);
     }
-    for(int j=0; j<MAX_CONNECTIONS; j++){
-        if(p4runtime[j].p4runtime_id != -1 && p4runtime[j].in_use==false){
+    for (int j = 0; j < MAX_CONNECTIONS; j++)
+    {
+        if (p4runtime[j].p4runtime_id != -1 && p4runtime[j].in_use == false)
+        {
             // change status of p4runtime
             p4runtime[j].in_use = true;
             available_runtimes--;
@@ -143,10 +155,13 @@ void link_add(int i){
     }
 }
 
-void link_del(int i){
+void link_del(int i)
+{
     // change status of p4runtime
-    for(int j=0; j<MAX_CONNECTIONS; j++){
-        if(p4runtime[j].p4runtime_id==session[i].p4runtime_id){
+    for (int j = 0; j < MAX_CONNECTIONS; j++)
+    {
+        if (p4runtime[j].p4runtime_id == session[i].p4runtime_id)
+        {
             p4runtime[j].in_use = false;
             available_runtimes++;
             break;
@@ -154,18 +169,23 @@ void link_del(int i){
     }
     // delete session
     session[i].p4runtime_id = -1;
+    // stop runtime
+    adjust_children(runtimes - 1);
 }
 
-void shm_start(void){
+void shm_start(void)
+{
     fd = open(SHM_NAME, O_RDWR);
-    if (fd < 0) {
+    if (fd < 0)
+    {
         syslog(LOG_ERR, "shm cannot open");
         exit(EXIT_FAILURE);
     }
     syslog(LOG_INFO, "fd: %d，SHM_SIZE: %d", fd, SHM_SIZE);
 
     base_ptr = mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 4096);
-    if (base_ptr == MAP_FAILED) {
+    if (base_ptr == MAP_FAILED)
+    {
         syslog(LOG_ERR, "shm cannot mmap");
         exit(EXIT_FAILURE);
     }
@@ -174,101 +194,130 @@ void shm_start(void){
     shm_ptr = base_ptr + META_AREA;
 }
 
-void shm_init(void){
+void shm_init(void)
+{
     session = (Connection *)(shm_ptr + SHM_SESSION_TABLE);
 
-    for(int i=0; i<MAX_CONNECTIONS; i++){
+    for (int i = 0; i < MAX_CONNECTIONS; i++)
+    {
         session[i].ovs_thread_id = -1;
         session[i].p4runtime_id = -1;
     }
 }
 
-void shm_end(void){
+void shm_end(void)
+{
     munmap(base_ptr, SHM_SIZE);
     close(fd);
 }
 
-void start_children(int num) {
-    if (num > MAX_CONNECTIONS) {
+void start_children(int num)
+{
+    if (num > MAX_CONNECTIONS)
+    {
         num = MAX_CONNECTIONS;
     }
 
-    for (; runtimes < num; runtimes++) {
+    for (; runtimes < num; runtimes++)
+    {
         pid_t pid = fork();
-        if (pid < 0) {
+        if (pid < 0)
+        {
             syslog(LOG_ERR, "fork failure");
             break;
-        } else if (pid == 0) {
+        }
+        else if (pid == 0)
+        {
             // 子プロセス
 
             execl(P4CTL, P4RUNCOMMAND, NULL);
             // execl failure時
             syslog(LOG_ERR, "execl failure");
             _exit(1);
-        } else {
+        }
+        else
+        {
             // 親プロセス
 
             // add p4runtime
-            for(int i=0; i<MAX_CONNECTIONS; i++){
-                if(p4runtime[i].p4runtime_id == -1) {
+            for (int i = 0; i < MAX_CONNECTIONS; i++)
+            {
+                if (p4runtime[i].p4runtime_id == -1)
+                {
                     p4runtime[i].p4runtime_id = pid;
                     p4runtime[i].in_use = false;
                     available_runtimes++;
                     break;
                 }
             }
-            syslog(LOG_INFO,"子プロセス %d 起動 (PID=%d)", runtimes, pid);
+            syslog(LOG_INFO, "子プロセス %d 起動 (PID=%d)", runtimes, pid);
         }
     }
 }
 
-void stop_all_children() {
-    for (int i = 0; i < runtimes; i++) {
-        if (p4runtime[i].p4runtime_id > 0) {
+void stop_all_children()
+{
+    for (int i = 0; i < runtimes; i++)
+    {
+        if (p4runtime[i].p4runtime_id > 0)
+        {
             kill(p4runtime[i].p4runtime_id, SIGTERM);
-            syslog(LOG_INFO,"子プロセス %d 終了 (PID=%d)", i, p4runtime[i].p4runtime_id);
+            syslog(LOG_INFO, "子プロセス %d 終了 (PID=%d)", i, p4runtime[i].p4runtime_id);
         }
     }
 
     // 終了待ち
-    for (int i = 0; i < runtimes; i++) {
-        if (p4runtime[i].p4runtime_id > 0) {
+    for (int i = 0; i < runtimes; i++)
+    {
+        if (p4runtime[i].p4runtime_id > 0)
+        {
             int status;
             waitpid(p4runtime[i].p4runtime_id, &status, 0);
         }
     }
 
     runtimes = 0;
-    for(int i=0; i<MAX_CONNECTIONS; i++){
+    for (int i = 0; i < MAX_CONNECTIONS; i++)
+    {
         p4runtime[i].p4runtime_id = -1;
-        p4runtime[i].in_use=false;
+        p4runtime[i].in_use = false;
     }
 }
 
-void adjust_children(int target) {
-    if (target == runtimes) {
+void adjust_children(int target)
+{
+    if (target == runtimes)
+    {
         return;
     }
 
-    if (target > MAX_CONNECTIONS) {
+    if (target > MAX_CONNECTIONS)
+    {
         target = MAX_CONNECTIONS;
     }
 
-    if (target > runtimes) {
+    if (target > runtimes)
+    {
         // add runtimes
         start_children(target);
-    } else if (target < runtimes) {
+    }
+    else if (target < runtimes)
+    {
         // delete runtimes
-        for (int i = runtimes - 1; i >= target; i--) {
-            if (p4runtime[i].p4runtime_id > 0 && p4runtime[i].in_use == false) {
-                if (kill(p4runtime[i].p4runtime_id, SIGTERM) == -1) {
+        for (int i = runtimes - 1; i >= target; i--)
+        {
+            if (p4runtime[i].p4runtime_id > 0 && p4runtime[i].in_use == false)
+            {
+                if (kill(p4runtime[i].p4runtime_id, SIGTERM) == -1)
+                {
                     syslog(LOG_ERR, "kill failure");
                 }
                 int status;
-                if (waitpid(p4runtime[i].p4runtime_id, &status, 0) == -1) {
+                if (waitpid(p4runtime[i].p4runtime_id, &status, 0) == -1)
+                {
                     syslog(LOG_ERR, "waitpid failure");
                 }
-                syslog(LOG_INFO,"子プロセス %d 終了 (PID=%d)", i, p4runtime[i].p4runtime_id);
+                syslog(LOG_INFO, "子プロセス %d 終了 (PID=%d)", i, p4runtime[i].p4runtime_id);
                 p4runtime[i].p4runtime_id = -1;
                 runtimes--;
             }
@@ -276,12 +325,15 @@ void adjust_children(int target) {
     }
 }
 
-void sigchld_handler(int signo) {
+void sigchld_handler(int signo)
+{
     (void)signo;
-    while (waitpid(-1, NULL, WNOHANG) > 0);
+    while (waitpid(-1, NULL, WNOHANG) > 0)
+        ;
 }
 
-void setup_signal_handlers() {
+void setup_signal_handlers()
+{
     struct sigaction sa;
     sa.sa_handler = sigchld_handler;
     sigemptyset(&sa.sa_mask);
@@ -289,13 +341,15 @@ void setup_signal_handlers() {
     sigaction(SIGCHLD, &sa, NULL);
 }
 
-void setup_server_socket() {
+void setup_server_socket()
+{
     struct sockaddr_un addr;
 
-    unlink(SOCKET_PATH);  // remove old sock file
+    unlink(SOCKET_PATH); // remove old sock file
 
     server_sock = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (server_sock < 0) {
+    if (server_sock < 0)
+    {
         syslog(LOG_ERR, "socket failure");
         exit(EXIT_FAILURE);
     }
@@ -304,36 +358,41 @@ void setup_server_socket() {
     addr.sun_family = AF_UNIX;
     strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
 
-    if (bind(server_sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    if (bind(server_sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
         syslog(LOG_ERR, "bind failure");
         close(server_sock);
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_sock, 5) < 0) {
+    if (listen(server_sock, 5) < 0)
+    {
         syslog(LOG_ERR, "listen failure");
         close(server_sock);
         exit(EXIT_FAILURE);
     }
 }
 
-void handle_client(int client_fd) {
+void handle_client(int client_fd)
+{
     char buffer[BUFFER_SIZE];
     char shutdown_cmd[1024];
     snprintf(shutdown_cmd, sizeof(shutdown_cmd), "pkill %s", TARGET_PROCESS);
 
     ssize_t n = read(client_fd, buffer, sizeof(buffer) - 1);
-    if (n <= 0) {
+    if (n <= 0)
+    {
         syslog(LOG_ERR, "read failure");
         close(client_fd);
         return;
     }
     buffer[n] = '\0';
 
-    syslog(LOG_INFO,"受信コマンド: %s", buffer);
+    syslog(LOG_INFO, "受信コマンド: %s", buffer);
 
     // コマンド解析例
-    if (strncmp(buffer, "start ", 6) == 0) {
+    if (strncmp(buffer, "start ", 6) == 0)
+    {
         // if (runtimes != 0) {
         //     dprintf(client_fd, "起動後はupdateコマンドを使用してください\n");
         //     close(client_fd);
@@ -345,60 +404,78 @@ void handle_client(int client_fd) {
         //     dprintf(client_fd, "  PID: %d\n", p4runtime[i].p4runtime_id);
         // }
         dprintf(client_fd, "すでに起動しています\n");
-    } else if (strncmp(buffer, "update ", 7) == 0) {
+    }
+    else if (strncmp(buffer, "update ", 7) == 0)
+    {
         int num = atoi(buffer + 7);
         adjust_children(num);
         dprintf(client_fd, "子プロセス数を %d 個に調整しました\n", num);
-    } else if (strncmp(buffer, "stop", 4) == 0) {
+    }
+    else if (strncmp(buffer, "stop", 4) == 0)
+    {
         stop_all_children();
         dprintf(client_fd, "子プロセスをすべて停止しました\n");
-    } else if (strncmp(buffer, "status", 6) == 0) {
+    }
+    else if (strncmp(buffer, "status", 6) == 0)
+    {
         dprintf(client_fd, "子プロセス数: %d\n", runtimes);
-        for (int i = 0; i < runtimes; i++) {
+        for (int i = 0; i < runtimes; i++)
+        {
             dprintf(client_fd, "  PID: %d\n", p4runtime[i].p4runtime_id);
         }
-    } else if (strncmp(buffer, "shutdown", 8) == 0) {
+    }
+    else if (strncmp(buffer, "shutdown", 8) == 0)
+    {
         dprintf(client_fd, "P4Launcherとすべての子プロセスを終了します\n");
         int ret = system(shutdown_cmd);
         dprintf(client_fd, "%d", ret);
-    } else {
+    }
+    else
+    {
         dprintf(client_fd, "不明なコマンド\n");
     }
     close(client_fd);
 }
 
-void daemonize(void) {
+void daemonize(void)
+{
     pid_t pid;
 
     // first fork
     pid = fork();
-    if (pid < 0) {
+    if (pid < 0)
+    {
         syslog(LOG_ERR, "fork failure");
         exit(EXIT_FAILURE);
     }
-    if (pid > 0) {
+    if (pid > 0)
+    {
         // 親プロセスは終了
         exit(EXIT_SUCCESS);
     }
 
     // セッションリーダになる
-    if (setsid() < 0) {
+    if (setsid() < 0)
+    {
         syslog(LOG_ERR, "setsid failure");
         exit(EXIT_FAILURE);
     }
 
     // second fork (端末から完全に切り離す)
     pid = fork();
-    if (pid < 0) {
+    if (pid < 0)
+    {
         syslog(LOG_ERR, "fork failure (2)");
         exit(EXIT_FAILURE);
     }
-    if (pid > 0) {
+    if (pid > 0)
+    {
         exit(EXIT_SUCCESS);
     }
 
     // cd /
-    if (chdir("/") < 0) {
+    if (chdir("/") < 0)
+    {
         syslog(LOG_ERR, "chdir failure");
         exit(EXIT_FAILURE);
     }
@@ -412,7 +489,8 @@ void daemonize(void) {
     close(STDERR_FILENO);
 
     int fd = open("/dev/null", O_RDWR);
-    if (fd != STDIN_FILENO) {
+    if (fd != STDIN_FILENO)
+    {
         syslog(LOG_ERR, "stdin open failure");
         exit(EXIT_FAILURE);
     }
