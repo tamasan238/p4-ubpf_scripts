d51bb92c3dbb79e31afcdd0cb0af735c7b4c758a
masaki <m1117@aol.jp>
2025-08-24 20:50:28 +0900

    add

---
+++ kvm/affinity.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pin_to_cpu() {
+    local cores=$1
+    local pattern=$2
+
+    for PID in $(pgrep -f "$pattern"); do
+        taskset -cp $cores $PID
+        for TID in $(ls /proc/$PID/task); do
+            taskset -cp $cores $TID
+        done
+    done
+}
+
+pin_to_cpu "0,1,2,3" "ovs-vswitchd"
+pin_to_cpu "4,5" "guest=p4"
+pin_to_cpu "6,7" "ivshmem-server"
+pin_to_cpu "6,7" "guest=user_dpdk"
