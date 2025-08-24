3f76c09ee447595330be8b0a68786f16110c6179
masaki <m1117@aol.jp>
2025-08-24 22:33:25 +0900

    update

--- kvm/priority.sh
+++ kvm/priority.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-for PID in $(pgrep ovs-vswitchd qemu-system-x86_64); do
+for PID in $(pgrep ivshmem); do
+    sudo chrt -f -p 99 $PID
+    chrt -p $PID
+done
+
+for PID in $(pgrep ovs-vswitchd); do
+    sudo chrt -f -p 99 $PID
+    chrt -p $PID
+done
+
+for PID in $(pgrep qemu-system-x86_64); do
     sudo chrt -f -p 99 $PID
     chrt -p $PID
 done
