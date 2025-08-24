8089d87851e4d51a8ce47a9447777625d72df8a2
masaki <m1117@aol.jp>
2025-08-24 21:12:25 +0900

    update

--- kvm/affinity.sh
+++ kvm/affinity.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-pin_to_cpu() {
-    local cores=$1
-    local pattern=$2
+pin_to_cpuset() {
+    local cgname=$1
+    local cores=$2
+    local pattern=$3
+
+    CGROUP_PATH="/sys/fs/cgroup/cpuset/$cgname"
+    sudo mkdir -p $CGROUP_PATH
+    echo $cores | sudo tee $CGROUP_PATH/cpuset.cpus
+    echo 0 | sudo tee $CGROUP_PATH/cpuset.mems
+    sudo bash -c 'echo 1 > '$CGROUP_PATH'/cpuset.cpu_exclusive' || true
 
     for PID in $(pgrep -f "$pattern"); do
-        taskset -cp $cores $PID
-        for TID in $(ls /proc/$PID/task); do
-            taskset -cp $cores $TID
-        done
+        echo $PID | sudo tee $CGROUP_PATH/tasks
     done
 }
 
-pin_to_cpu "0,1,2,3" "ovs-vswitchd"
-pin_to_cpu "4,5" "guest=p4"
-pin_to_cpu "6,7" "ivshmem-server"
-pin_to_cpu "6,7" "guest=user_dpdk"
+pin_to_cpuset "ovs_vswitchd" "0-3" "ovs-vswitchd"
+pin_to_cpuset "guest_p4" "4-5" "guest=p4"
+pin_to_cpuset "ivshmem_server" "6" "ivshmem-server"
+pin_to_cpuset "guest_user_dpdk" "6-7" "guest=user_dpdk"
