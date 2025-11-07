7d056f56a40c6c8dfa07519fd3c114ddb2baf49c
Masaki Iwai <m1117@aol.jp>
2025-11-07 23:16:58 +0900

    update

--- p4/p4launcher/p4l_server.c
+++ p4/p4launcher/p4l_server.c
@@ -38,7 +38,7 @@ typedef struct {
 #define SHM_SESSION_TABLE 0
 #define SHM_TABLE_IS_LOCKED (SHM_SESSION_TABLE + sizeof(Connection) * MAX_CONNECTIONS)
 
-#define MAX_CONNECTIONS 32
+#define MAX_CONNECTIONS 512
 #define INIT_CLIENTS 1
 
 #define UBPF_RUNTIME_PATH "/home/iwai/ubpf/build/bin/ubpf_test"
