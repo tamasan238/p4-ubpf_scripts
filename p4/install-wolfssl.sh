32f9877447b66b333ae52f6a07c32a68f68793b6
Masaki I. <m1117@aol.jp>
2025-11-15 20:00:53 +0900

    update

--- p4/install-wolfssl.sh
+++ p4/install-wolfssl.sh
@@ -2,8 +2,9 @@ git clone https://github.com/wolfssl/wolfssl
 cd wolfssl
 git checkout v5.8.2-stable
 ./autogen.sh
-./configure --enable-aesni
+./configure --enable-intelasm --enable-sp-asm
 make -j$(nproc)
 make test
 sudo make install
 sudo ldconfig
+./wolfcrypt/benchmark/benchmark
